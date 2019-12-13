eefc31c57b4ea7a51233daae447286f72b754da2
Maycon <32937928+Maycon-Ferreira@users.noreply.github.com>
2019-12-13 17:42:17 -0300

    Basic input reading

--- src/eco.c
+++ src/eco.c
@@ -17,9 +17,48 @@ size_t C = 0;
 eco_object_t **ecosystem = NULL;
 __uint8_t **candidates = NULL;
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err34-c"
+
+// @TODO safe input read
 int eco_read_input(char *filepath) {
+    FILE *file = fopen(filepath, "r");
+
+    if (file == NULL) {
+        return 2;
+    }
+
+    fscanf(file, "%d %d %d %d %zu %zu", &GEN_RABBIT_PROC, &GEN_FOX_PROC, &GEN_FOX_STARVE, &MAX_GEN_COUNT, &L, &C);
+
+    ecosystem = calloc(L, sizeof(eco_object_t *));
+    candidates = calloc(L, sizeof(__uint8_t *));
+
+    for (int i = 0; i < L; ++i) {
+        ecosystem[i] = calloc(C, sizeof(eco_object_t *));
+        candidates[i] = calloc(C, sizeof(__uint8_t *));
+    }
+
+    int object_count = 0;
+
+    fscanf(file, "%d\n", &object_count);
+
+    int i, j;
+    char object[7] = {0};
+
+    for (int k = 0; k < object_count; ++k) {
+        fscanf(file, "%6s %d %d\n", object, &i, &j);
+
+        if (strcmp(object, "COELHO") == 0) { ecosystem[i][j].rabbit.type = RABBIT; }
+        else if (strcmp(object, "ROCHA") == 0) { ecosystem[i][j].rock.type = ROCK; }
+        else if (strcmp(object, "RAPOSA") == 0) { ecosystem[i][j].fox.type = FOX; }
+    }
+
+    fclose(file);
+
     return 0;
 }
+
+#pragma clang diagnostic pop
 
 int eco_sim_step() {
     if (step_count == MAX_GEN_COUNT) { return -1; }
